a0cd039701c216a0037ae210543d69385e0ea2ac
maximilian99-ai <maximilian99@icloud.com>
2024-02-16 22:51:00 +0900

    first commit

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail 
+      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_München.jpg/1280px-Stadtbild_München.jpg'
+      title='First Meetup'
+      address='Some address 5, Some City'
+      description='This is a first meetup'
+    />
+  );
+}
+
+export default MeetupDetails;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+// our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetupPage() {
+  const addMeetupHandler = () => {
+    
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
